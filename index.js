cb8b09ef71673d0ca55768c82e7794da42269306
Hiba Ali <hiba.mumin22@gmail.com>
2022-04-19 15:33:57 -0500

    questions array with authors name and title for readme

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
+// Create an array of questions for user input
+const questions = [
+
+    {
+        type: "input",
+        name: "Author",
+        message: "Enter author's (your) name:",
+      },
+
+    {
+        type: 'input',
+        message: "Enter a title for your ReadMe:",
+        name: 'Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid title is required.");
+            }
+            return true;
+        }
+    },
+
 
 
 
