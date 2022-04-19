0f3fe0af686bd0bd57c0bd11d72f4139d2b2ca8d
Hiba Ali <hiba.mumin22@gmail.com>
2022-04-19 15:33:19 -0500

    imported packages and utils file

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+//Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
 
