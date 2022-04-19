5c291cab035ff8b8eecf710862623bf38b9550f1
Hiba Ali <hiba.mumin22@gmail.com>
2022-04-19 15:48:06 -0500

    added usage and license content

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,6 +11,30 @@ function docuGenerator (userInput) {
     ${userInput.Description}
     ***
 
+    # Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+***
+
+
+## Installation
+${userInput.Installation}
+
+
+
+## Usage
+${userInput.Usage}
+
+
+## License
+${licenseBadges(userInput)}
+## Contributing
+If you would like to contribute to this project, please [add me on GitHub](https://github.com/${userInput.Contributing}).
+
 
 
 
